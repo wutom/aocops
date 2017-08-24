638576965952478f99a0490de271ed83968f299e
tom <wutom@163.com>
2017-08-24 18:16:40 +0800

    change mat is chartkick

--- index/views.py
+++ index/views.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import json
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader, RequestContext
@@ -6,6 +7,7 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 ###app models
 from index.models import aocops_fileImage, aocops_indexType, aocops_indexList, aocops_indexBulletin
 from vminfo.models import host_info, app_info
+from vmset.models import info_label, info_manager, info_group, info_status, app_info_alarm
 from idcinfo.models import idc_contact, idc_types, idc_info, cabinet_info, device_type, device_info
 
 #### Create your views here.
@@ -21,7 +23,8 @@ from django.utils.http import urlquote
 from django.db.models import Q
 
 ####加载自定义模块
-from api.python import matp
+####弃用matplotlib 绘图，采用chartkick
+#from api.python import matp
 
 #导航栏部分已全局变量的方式返回给具体视图
 
@@ -99,6 +102,7 @@ def index(request):
 	
 #####数据中心 类型统计-------------------------------------------------------
 	idc_types_list = idc_types.objects.all()
+	idc_count = idc_info.objects.all().count()
 	idc_label_count = {}
 	#i = 1
 	for itl in idc_types_list:
@@ -106,7 +110,7 @@ def index(request):
 		idc_label_count[itl.name] = idc_info.objects.filter(types_name_id=int(itl.id)).count()
 	#	i = i + 1
 
-	##遍历idc_label_count 数据类型和类型统计数
+	##遍历idc_label_count 数据类型和类型统计数 弃用
 	idc_label = []
 	idc_types_count = []
 
@@ -114,25 +118,15 @@ def index(request):
 		idc_label.append(itc.name)
 		idc_types_count.append(idc_info.objects.filter(types_name_id=int(itc.id)).count())
 
-###------绘图部分--------##
-##汇总数据中心报表图数据
-	data_figsizes = (2.5,2)
-
-	idc_path = 'static/img/datacenter.png'
-	idc_lab = matp.matp_label(idc_label)
-	idc_siz = matp.matp_size(idc_types_count)
-	figs = matp.matp_figsize(data_figsizes)
-	matp.matp_pie(idc_lab,idc_siz,figs,idc_path)
-
-
 #####设备汇总 类型统计-------------------------------------------------------
 	dev_types_list = device_type.objects.all()
+	dev_count = device_info.objects.all().count()
 	dev_label_count = {}
 
 	for dtl in dev_types_list:
 		dev_label_count[dtl.name] = device_info.objects.filter(types_id=int(dtl.id)).count()
 
-	##遍历dev_label_count 数据类型和类型统计数
+	##遍历dev_label_count 数据类型和类型统计数 弃用
 	dev_label = []
 	dev_types_count = []
 
@@ -140,18 +134,10 @@ def index(request):
 		dev_label.append(dtc.name)
 		dev_types_count.append(device_info.objects.filter(types_id=int(dtc.id)).count())
 
-###------------绘图部分----------------##
-##汇总物理设备报表图数据
-
-	dev_path = 'static/img/devicecount.png'
-	dev_lab = dev_label
-	dev_siz = dev_types_count
-	figs = data_figsizes
-	matp.matp_pie(dev_lab,dev_siz,figs,dev_path)
-
 
 #####主机汇总 类型统计-------------------------------------------------------
 	host_types_list = host_info.objects.values('vm_types').distinct()
+	host_count = host_info.objects.all().count()
 	host_label_count = {}
 	host_label = []
 	host_types_count = []
@@ -159,19 +145,24 @@ def index(request):
 	for htl in host_types_list:
 		name_types = htl['vm_types']
 		host_label_count[name_types] = host_info.objects.filter(vm_types=name_types).count()
-	##遍历host_label_count 数据类型和类型统计数
+	##遍历host_label_count 数据类型和类型统计数 弃用
 		host_label.append(name_types)
 		host_types_count.append(host_info.objects.filter(vm_types=name_types).count())
 
-###---------绘图部分--------##
-##汇总物理设备报表图数据
+#####项目汇总 类型统计
+	label_types_list = info_label.objects.all()
+	label_all_count = info_label.objects.all().count()
+	label_count = {}
 
-	dev_path = 'static/img/hostcount.png'
-	host_lab = host_label
-	host_siz = host_types_count
-	figs = data_figsizes
-	matp.matp_pie(host_lab,host_siz,figs,dev_path)
+	for ltl in label_types_list:
+		name_label = str(ltl)
+		label_count[name_label] = host_info.objects.filter(vm_label=int(ltl.id)).count()
 
+####测试数据
+	#data = {'Chrome': 52.9, 'Opera': 1.6, 'Firefox': 27.7}
+	data = [{'data': [[u'项目1', 52.9], ['项目2', 50.7]], 'name': u'物理机'},
+ {'data': [[u'项目1', 27.7], ['项目2', 25.9]], 'name': u'虚拟机'}]
+	value = [{'data':[['2013-04-01 00:09:00 UTC',20],['2013-04-01 00:10:00 UTC',5],['2013-04-01 00:11:00 UTC',25],['2013-04-01 00:12:00 UTC',50],['2013-04-01 00:13:00 UTC',10],['2013-04-01 00:14:00 UTC',15],['2013-04-01 00:15:00 UTC',35]],'name':'Chrome'}]
 ##################
 	bull = aocops_indexBulletin.objects.all()
 	bul = {}
@@ -188,7 +179,14 @@ def index(request):
 		'idc_label_count' : idc_label_count,
 		'dev_label_count' : dev_label_count,
 		'host_label_count' : host_label_count,
-		'username' : username
+		'idc_count' : idc_count,
+		'dev_count' : dev_count,
+		'host_count' : host_count,
+		'label_count' :label_count,
+		'label_all_count' : label_all_count,
+		'username' : username,
+		'data' : data,
+		'value' : value,
 		},processors=[index_list])
 	return HttpResponse(template.render(context))
 	
@@ -412,12 +410,16 @@ def device(request, id):
 '''
 下面未测试代码部分
 '''
+
+###
+
 ## 获取请求数据
 def dnsfind(request):
     return render_to_response('index/dnsfind.html')
  
 ## 接收请求数据
-def search(request):  
+def search(request):
+	
     ctx = {}
     if request.method == "GET":
     	ctx['domain'] = request.GET['domain']
@@ -425,7 +427,7 @@ def search(request):
     	cmdd="python /opt/aocdev/aocops/index/dnsfind.py %s %s" % (ctx['domain'], ctx['dnsip'])
     	dns=os.popen(cmdd).read()
     template = loader.get_template('index/search.html')
-    dn = {'dns':dns} 
+    dn = {'dns':dns}
     #return HttpResponse(template.render(dn))
     return HttpResponse(dns)
 
